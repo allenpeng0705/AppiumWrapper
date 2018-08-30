1fef9d28797fe51c3aa3c450cf37968b2771ee8f
MapleShao <939078792@qq.com>
2018-08-30 15:37:59 +0800

    update .gitignore

--- EMAAppiumClient/emaappiumclient/tests/ema/query.py
+++ EMAAppiumClient/emaappiumclient/tests/ema/query.py
@@ -1,4 +1,7 @@
 class query(object):
+    def app_permission_allow(self):
+        pass
+
     def login_welcome_loginBtn(self):
         pass
 
@@ -18,4 +21,30 @@ class query(object):
         pass
 
     def login_login_pwd(self):
+        pass
+
+    # top bar
+    def top_bar_menu(self):
+        pass
+
+    def top_bar_switch(self):
+        pass
+
+    def top_bar_filter(self):
+        pass
+
+    # left menu view
+    def menu_messages_item(self):
+        pass
+
+    def menu_events_item(self):
+        pass
+
+    def menu_local_item(self):
+        pass
+
+    def menu_calendar_item(self):
+        pass
+
+    def menu_settings_item(self):
         pass
